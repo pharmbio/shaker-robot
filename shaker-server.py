6d984072d359de83fd9c04e7a6123578488e3bd0
andersla <anders@ormbunkar.se>
2020-01-28 10:03:25 +0000

    add comments, refactoring

--- shaker-server.py
+++ shaker-server.py
@@ -5,32 +5,41 @@ import time
 import collections
 from flask import Flask, json
 
+# Define GPIO pins
 START_STOP = 20
 SPEED_UP = 21
 SPEED_DOWN = 16
 SPEED_SENSOR = 4
 
+# Setup GPIO
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(START_STOP, GPIO.OUT)
 GPIO.setup(SPEED_UP, GPIO.OUT)
 GPIO.setup(SPEED_DOWN, GPIO.OUT)
 GPIO.setup(SPEED_SENSOR, GPIO.IN,  pull_up_down = GPIO.PUD_DOWN)
 
+# Set outputs down to start with
 GPIO.output(START_STOP, 0)
 GPIO.output(SPEED_UP, 0)
 GPIO.output(SPEED_DOWN, 0)
 
+# Storage for the latest timestamps from the speed sensor
 stack = collections.deque(maxlen=5)
 
-def press(pin_no):
-  print("press: " + str(pin_no))
-  GPIO.output(pin_no, 1)
-  time.sleep(1)
-  GPIO.output(pin_no, 0)
-
+# callback method used when speed sensor pin rises
 def speed_sensor_rising(channel):
   stack.appendleft(time.time())
 
+# simulate button press by putting current through pin for x seconds
+def press(pin_no):
+  # print("press: " + str(pin_no))
+  PRESS_TIME = 1
+  GPIO.output(pin_no, 1)
+  time.sleep(PRESS_TIME)
+  GPIO.output(pin_no, 0)
+
+# return current shaker speed
+# calculates time between last two readings from speed sensor
 def get_speed():
   current = time.time()
 
